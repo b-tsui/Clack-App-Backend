2c59c98a2810b03ae9e4f3a8fd0ba7a93707c630
Brandon Tsui <itstsui@gmail.com>
2020-04-24 17:15:26 -0400

    updated channel post route

--- routes/channels.js
+++ routes/channels.js
@@ -67,7 +67,7 @@ router.post('/', requireAuth, asyncHandler(async (req, res) => {
     const { userId, name, isDM, } = req.body;
     const channel = await Channel.create({ userId, name, isDM });
     const channelUsers = await ChannelUser.create({ userId, channelId: channel.dataValues.id })
-    res.status(204).json({ channel });
+    res.status(204).json({ channelId: channel.dataValues.id });
 }));
 
 //updates a channel name, checks to see if user's id matches with "owner" of channel
